66caae8a93807b2124d9ce053206ae82aff465ca
rscibelli <scibellrob@gmail.com>
2023-04-30 17:18:52 -0400

    change baseur;

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,5 @@
-// const baseUrl = "http://10.0.0.251:8080"
-const baseUrl = "http://localhost:8080"
+const baseUrl = "http://10.0.0.251:8080"
+// const baseUrl = "http://localhost:8080"
 
 export const createSongRequest = (data) => {
     const options = {
